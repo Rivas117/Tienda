9f65160c15e61ab6a6039081bfe0c3290f49bd79
Rivas117 <167035998+Rivas117@users.noreply.github.com>
2024-12-02 22:19:53 -0600

    Carrito + Handlebars en Templates

---
+++ showCarrito.php
@@ -0,0 +1,13 @@
+<?php
+/*Tipo Handlebars*/
+include "global/config.php";
+include "carrito.php";
+include "templates/header.php";
+?>
+
+<br>
+<h3>Lista del Carrito</h3>
+
+<?php
+include "templates/footer.php"
+?>
